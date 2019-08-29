9bc4ed8073c8c07edcab2c05b6285bd7218b7dab
0711709 <0711709@gmail.com>
2019-08-29 09:45:53 +0800

    add recommend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of WeekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="img" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      WeekendList: [
+        {
+          id: '001',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+          title: '广东科学中心',
+          desc: '15612条评论'
+        },
+        {
+          id: '002',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+          title: '广东科学中心',
+          desc: '15612条评论'
+        },
+        {
+          id: '003',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+          title: '广东科学中心',
+          desc: '15612条评论'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/mixins.styl"
+.title
+  margin-top: 0.2rem
+  line-height: 0.8rem
+  background: #eeeeee
+  text-indent: 0.2rem
+.item-img-wrapper
+  overflow: hidden
+  height: 0
+  padding-bottom: 33.9%
+  .item-img
+    width: 100%
+.item-info
+  padding: 0.1rem
+  .item-title
+    line-height: 0.54rem
+    font-size: 0.32rem
+    ellipsis()
+  .item-desc
+    line-height: 0.4rem
+    color: #ccc
+    ellipsis()
+</style>
